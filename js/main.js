7d9500313df62bfea17bbaa399178e2d12b4b351
Mathias <math86s0@stud.kea.dk>
2025-03-31 16:19:55 +0200

    Swagger

--- js/main.js
+++ js/main.js
@@ -1,76 +1,209 @@
-document.querySelector("#hue").addEventListener("mouseover", hueHighlight);
-document.querySelector("#hue").addEventListener("mouseout", hueUnhighlight);
-document.querySelector("#hue").addEventListener("click", hueInfo);
-document.querySelector("#voks").addEventListener("mouseover", voksHighlight);
-document.querySelector("#voks").addEventListener("mouseout", voksUnhighlight);
-document.querySelector("#voks").addEventListener("click", voksInfo);
-document.querySelector("#trimmer").addEventListener("mouseover", trimmerHighlight);
-document.querySelector("#trimmer").addEventListener("mouseout", trimmerUnhighlight);
-document.querySelector("#trimmer").addEventListener("click", trimmerInfo);
+document.querySelector("#hue").addEventListener("mouseover", highlight);
+document.querySelector("#hue").addEventListener("mouseout", unhighlight);
+document.querySelector("#hue").addEventListener("click", info);
+document.querySelector("#voks").addEventListener("mouseover", highlight);
+document.querySelector("#voks").addEventListener("mouseout", unhighlight);
+document.querySelector("#voks").addEventListener("click", info);
+document.querySelector("#trimmer").addEventListener("mouseover", highlight);
+document.querySelector("#trimmer").addEventListener("mouseout", unhighlight);
+document.querySelector("#trimmer").addEventListener("click", info);
 
-function hueHighlight() {
-    console.log("Hue på");
-    document.querySelector("#hue").style.opacity = "0.3";
+document.querySelector("#skæg").addEventListener("mouseout", unhighlight)
+document.querySelector("#skæg").addEventListener("click", fjol);
+
+function highlight(){
+    console.log(this);
+    this.style.opacity = "0.3";
 }
 
-function hueUnhighlight() {
-    console.log("Hue af");
-    document.querySelector("#hue").style.opacity= "1";
-    document.querySelector("#hue2").style.opacity= "0";
+function unhighlight(){
+    console.log(this.id + " af");
 
+    if(this == hue){
+        document.querySelector("#hue").style.opacity = "1";
+        document.querySelector("#hue2").style.opacity = "0";
+        document.getElementById('img-container-1').src="";
+        document.getElementById('img-container-2').src="";
+        document.getElementById('img-container-3').src="";
+        document.getElementById('time').className = "info-box unselected";
+        document.getElementById('difficulty').className = "info-box unselected";
+        document.getElementById('efficiency').className = "info-box unselected";
+        document.querySelector( ".info-text > h2").textContent = "Tip!"
+        document.querySelector(".info-text > article > p").textContent = "Klik på et ikon"
+    }
+    else if(this == voks){
+        document.querySelector("#voks").style.opacity = "1";
+        document.querySelector("#strit1").style.opacity = "1";
+        document.querySelector("#strit2").style.opacity = "1";
+        document.querySelector("#strit3").style.opacity = "1";
+        document.querySelector("#strit4").style.opacity = "1";
+        document.getElementById('img-container-1').src="";
+        document.getElementById('img-container-2').src="";
+        document.getElementById('img-container-3').src="";
+        document.getElementById('time').className = "info-box unselected";
+        document.getElementById('difficulty').className = "info-box unselected";
+        document.getElementById('efficiency').className = "info-box unselected";
+        document.querySelector( ".info-text > h2").textContent = "Tip!"
+        document.querySelector(".info-text > article > p").textContent = "Klik på et ikon"
+    }
+    else if (this == trimmer){
+        document.querySelector("#trimmer").style.opacity = "1";
+        document.querySelector("#hår").style.opacity = "1";
+        document.querySelector("#afklip").style.opacity = "0";
+        document.getElementById('img-container-1').src="";
+        document.getElementById('img-container-2').src="";
+        document.getElementById('img-container-3').src="";
+        document.getElementById('time').className = "info-box unselected";
+        document.getElementById('difficulty').className = "info-box unselected";
+        document.getElementById('efficiency').className = "info-box unselected";
+        document.querySelector( ".info-text > h2").textContent = "Tip!"
+        document.querySelector(".info-text > article > p").textContent = "Klik på et ikon"
+    }
+    else if (this == skæg){
+        document.querySelector("#skæg").classname = "fadeIn";
+    }
 }
 
-function hueInfo() {
-    console.log("hueInfo");
-    document.querySelector( ".info-text > h2").textContent = "Hue!"
-    document.querySelector(".info-text > article > p").textContent = "lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum "
-    document.querySelector("#hue2").style.opacity = "1";
+function info(){
+    console.log(this.id);
+
+    if(this == hue){
+        document.querySelector( ".info-text > h2").textContent = "Hue!";
+        document.getElementById("img-container-1").src="img/star-1.png";
+        document.getElementById('time').className = "info-box green";
+        document.getElementById('img-container-2').src="img/star-1.png";
+        document.getElementById('difficulty').className = "info-box green";
+        document.getElementById('img-container-3').src="img/star-5.png";
+        document.getElementById('efficiency').className = "info-box red";
+        document.querySelector(".info-text > article > p").textContent = "lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum "
+        document.querySelector("#hue2").style.opacity = "1";
+    }
+    else if(this == voks){
+        document.querySelector("#strit1").style.opacity = "0.0";
+        document.querySelector("#strit2").style.opacity = "0.0";
+        document.querySelector("#strit3").style.opacity = "0.0";
+        document.querySelector("#strit4").style.opacity = "0.0";
+        document.getElementById("img-container-1").src="img/star-5.png";
+        document.getElementById('time').className = "info-box red";
+        document.getElementById('img-container-2').src="img/star-1.png";
+        document.getElementById('difficulty').className = "info-box green";
+        document.getElementById('img-container-3').src="img/star-4.png";
+        document.getElementById('efficiency').className = "info-box yellow";
+        document.querySelector( ".info-text > h2").textContent = "Voks!"
+        document.querySelector(".info-text > article > p").textContent = "lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum "
     
+    }
+    else if(this == trimmer){
+        // document.querySelector("#hår").style.opacity = "0.0";
+        this.classList.add("wiggle");
+        document.querySelector("#hår").classList.add("fadeOut");
+        document.querySelector("#afklip").style.opacity = "1";
+        document.getElementById("img-container-1").src="img/star-5.png";
+        document.getElementById('time').className = "info-box red";
+        document.getElementById('img-container-2').src="img/star-1.png";
+        document.getElementById('difficulty').className = "info-box green";
+        document.getElementById('img-container-3').src="img/star-5.png";
+        document.getElementById('efficiency').className = "info-box red";
+        document.querySelector( ".info-text > h2").textContent = "Trimmer!"
+        document.querySelector(".info-text > article > p").textContent = "lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum "
+        }
 }
 
-function voksHighlight() {
-    console.log("Voks på");
-    document.querySelector("#voks").style.opacity = "0.3";
-}
+// function hueUnhighlight() {
+//     console.log("Hue af");
+//     document.querySelector("#hue").style.opacity = "1";
+//     document.querySelector("#hue2").style.opacity = "0";
+//     document.getElementById('img-container-1').src="";
+//     document.getElementById('img-container-2').src="";
+//     document.getElementById('img-container-3').src="";
+//     document.getElementById('time').className = "info-box unselected";
+//     document.getElementById('difficulty').className = "info-box unselected";
+//     document.getElementById('efficiency').className = "info-box unselected";
+//     document.querySelector( ".info-text > h2").textContent = "Tip!"
+//     document.querySelector(".info-text > article > p").textContent = "Klik på et ikon"
 
-function voksUnhighlight() {
-    console.log("Voks af");
-    document.querySelector("#voks").style.opacity = "1";
-    document.querySelector("#strit1").style.opacity = "1";
-    document.querySelector("#strit2").style.opacity = "1";
-    document.querySelector("#strit3").style.opacity = "1";
-    document.querySelector("#strit4").style.opacity = "1";
-}
+// }
 
-function voksInfo() {
-    console.log("voksInfo");
-    document.querySelector("#strit1").style.opacity = "0.0";
-    document.querySelector("#strit2").style.opacity = "0.0";
-    document.querySelector("#strit3").style.opacity = "0.0";
-    document.querySelector("#strit4").style.opacity = "0.0";
-    document.querySelector( ".info-text > h2").textContent = "Voks!"
-    document.querySelector(".info-text > article > p").textContent = "lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum "
+// function hueInfo() {
+//     console.log("hueInfo");
+//     document.querySelector( ".info-text > h2").textContent = "Hue!";
+//     document.getElementById("img-container-1").src="img/star-1.png";
+//     document.getElementById('time').className = "info-box green";
+//     document.getElementById('img-container-2').src="img/star-1.png";
+//     document.getElementById('difficulty').className = "info-box green";
+//     document.getElementById('img-container-3').src="img/star-5.png";
+//     document.getElementById('efficiency').className = "info-box red";
+//     document.querySelector(".info-text > article > p").textContent = "lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum "
+//     document.querySelector("#hue2").style.opacity = "1";
     
-}
+// }
 
-function trimmerHighlight() {
-    console.log("Trimmer på");
-    document.querySelector("#trimmer").style.opacity = "0.3";
+// function voksUnhighlight() {
+//     console.log("Voks af");
+//     document.querySelector("#voks").style.opacity = "1";
+//     document.querySelector("#strit1").style.opacity = "1";
+//     document.querySelector("#strit2").style.opacity = "1";
+//     document.querySelector("#strit3").style.opacity = "1";
+//     document.querySelector("#strit4").style.opacity = "1";
+//     document.getElementById('img-container-1').src="";
+//     document.getElementById('img-container-2').src="";
+//     document.getElementById('img-container-3').src="";
+//     document.getElementById('time').className = "info-box unselected";
+//     document.getElementById('difficulty').className = "info-box unselected";
+//     document.getElementById('efficiency').className = "info-box unselected";
+//     document.querySelector( ".info-text > h2").textContent = "Tip!"
+//     document.querySelector(".info-text > article > p").textContent = "Klik på et ikon"
+// }
+
+// function voksInfo() {
+//     console.log("voksInfo");
+//     document.querySelector("#strit1").style.opacity = "0.0";
+//     document.querySelector("#strit2").style.opacity = "0.0";
+//     document.querySelector("#strit3").style.opacity = "0.0";
+//     document.querySelector("#strit4").style.opacity = "0.0";
+//     document.getElementById("img-container-1").src="img/star-5.png";
+//     document.getElementById('time').className = "info-box red";
+//     document.getElementById('img-container-2').src="img/star-1.png";
+//     document.getElementById('difficulty').className = "info-box green";
+//     document.getElementById('img-container-3').src="img/star-4.png";
+//     document.getElementById('efficiency').className = "info-box yellow";
+//     document.querySelector( ".info-text > h2").textContent = "Voks!"
+//     document.querySelector(".info-text > article > p").textContent = "lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum "
     
-}
+// }
 
-function trimmerUnhighlight() {
-    console.log("Trimmer af");
-    document.querySelector("#trimmer").style.opacity = "1";
-    document.querySelector("#hår").style.opacity = "1";
-    document.querySelector("#afklip").style.opacity = "0";
-}
+// function trimmerUnhighlight() {
+//     console.log("Trimmer af");
+//     document.querySelector("#trimmer").style.opacity = "1";
+//     document.querySelector("#hår").style.opacity = "1";
+//     document.querySelector("#afklip").style.opacity = "0";
+//     document.getElementById('img-container-1').src="";
+//     document.getElementById('img-container-2').src="";
+//     document.getElementById('img-container-3').src="";
+//     document.getElementById('time').className = "info-box unselected";
+//     document.getElementById('difficulty').className = "info-box unselected";
+//     document.getElementById('efficiency').className = "info-box unselected";
+//     document.querySelector( ".info-text > h2").textContent = "Tip!"
+//     document.querySelector(".info-text > article > p").textContent = "Klik på et ikon"
+// }
 
-function trimmerInfo() {
-    console.log("trimmerInfo");
-    document.querySelector("#hår").style.opacity = "0.0";
-    document.querySelector("#afklip").style.opacity = "1";
-    document.querySelector( ".info-text > h2").textContent = "Trimmer!"
-    document.querySelector(".info-text > article > p").textContent = "lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum "
+// function trimmerInfo() {
+//     console.log("trimmerInfo");
+//     document.querySelector("#hår").style.opacity = "0.0";
+//     document.querySelector("#afklip").style.opacity = "1";
+//     document.getElementById("img-container-1").src="img/star-5.png";
+//     document.getElementById('time').className = "info-box red";
+//     document.getElementById('img-container-2').src="img/star-1.png";
+//     document.getElementById('difficulty').className = "info-box green";
+//     document.getElementById('img-container-3').src="img/star-5.png";
+//     document.getElementById('efficiency').className = "info-box red";
+//     document.querySelector( ".info-text > h2").textContent = "Trimmer!"
+//     document.querySelector(".info-text > article > p").textContent = "lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum "
     
+// }
+
+function fjol() {
+    this.classList.add("fadeOut");
+    document.querySelector( ".info-text > h2").textContent = "Skæg!";
+    document.querySelector( ".info-text > article > p").textContent = "Fuck jeg har grimt skæg";
 }
